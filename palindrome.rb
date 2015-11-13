d7eec9858604c17dc0836fd048cd4c87e6ca8eb6
shun4shun3 <shunshun.43@gmail.com>
2015-11-13 12:38:03 +0900

    with comment

--- palindrome.rb
+++ palindrome.rb
@@ -13,3 +13,6 @@ def palindrome?(sentence)
   downcase_stripped_sentence = sentence.downcase.gsub(" ", "")
   downcase_stripped_sentence  ==  downcase_stripped_sentence.reverse
 end
+
+# 名前の付け方が違うんか。
+# if x == x true end はいらない。
